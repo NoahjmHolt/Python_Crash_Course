456b2db09c5195ea2d60518a83eea45913a39d8b
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2025-09-21 16:18:16 -0600

    Notes page to return to
    Created a Notes file to write down different things and return back to when needed

---
+++ Notes.py
@@ -0,0 +1,68 @@
+'''
+
+This will be important, a Notes page
+
+All files will have a start section like this that is a what we are doing,
+What I remember or may be thinking from the prompt
+
+Will included the prompts once things begin getting complicated
+
+Lastly, maybe steps taken to complete, mindset throughout the process
+and maybe errors ran into and solutions found
+
+'''
+
+# outline of each chapter Top notes
+'''
+Chapter #: title
+
+What its about
+
+Prompts and project notes
+
+Errors and fixes
+
+'''
+
+# Notes and thoughts by chapter
+
+'''
+
+The book: Python Crash Course by Eric Matthes and No Starch Press
+
+No Starch Press is one of my favorite companies for coding books,
+Sadly, I am pretty sure it is due to the texture of the book covers and the cartoons on the front
+
+that aside, the have MANY python books from the basics like this, to scripting and data,
+and the one I am most excited for, impractical projects.
+I am excited for that one to see if it has out of the box things that require some 
+fun projects to think through and change some views.
+
+No skipping ahead. the basics are most important
+
+'''
+
+# Chap 1
+# forgot pycharm is weird and if there is no space in front of the
+# start of the words, it throws a spelling error (**cough cough** dumb **cough cough**)
+'''
+
+Chapter 1: Python install and Hello world
+
+installing is easy, you can go to python.org or just search on Mac (which I am on) for your python version
+or install the latest version with brew python
+
+for hello world, print is auto included so no extra libraries and can call outside a function and run it in the terminal.
+
+'''
+
+# Chap 2
+'''
+
+Chapter 2: Variables and Simple Data Types
+
+what is a variable, what they hold, what can you do with them,
+and intro to data types for beginners.
+
+'''
+
