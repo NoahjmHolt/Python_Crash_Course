0f8fbc5ee83cc44905afe0577fc924e6fec1e8dc
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2025-09-21 16:04:26 -0600

    Hello World
    Back to the basics, always starts at the begining with Hello World, no matter the language you are learning to code in.
    
    Left comments with thoughts and reminders here

---
+++ hello_python.py
@@ -0,0 +1,25 @@
+
+'''
+
+Like all computer courses, the book starts "Hello World"
+
+Not too much to learn there but as a refresh and restart,
+good to start with the basics and see if I missed anything the first time through.
+
+I know the first time through I was 3 years in to college and was never the best student
+so probably missed  something or thought I knew something or ignored some of the basics
+thinking I knew better than I did.
+
+'''
+
+
+# easy enough in python, the Print Function
+
+# not needing any libraries or even a function,
+# to print you simply say print(what you want to say
+
+print("Hello World")
+
+# Good to know, in python the line ender (normally a ;)
+# is optional and so lines end with ; or with a new line character
+
