57a7de6e2a758747faef197ff743fc310d771865
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2025-09-24 18:50:54 -0600

    Chapter 2 wrap up
    All done with chapter 2 things.
    
    Only one error gotten, needed to cast an int to a string in order to print out alongside other strings so had to look that one up
    Solution: the str(#) casts to a string

--- chap2TIY.py
+++ chap2TIY.py
@@ -14,7 +14,50 @@ print(changeIt)
 '''
 
 # next block
+'''
 # 2-3
-name = "Jeriffer"   # this is the name combo of jeremy and jenifer
-print('Hello, ' + name + " would you like to learn some Python?")
+name = "JeriFFer"   # this is the name combo of jeremy and jenifer
+# print('Hello, ' + name + " would you like to learn some Python?")
+
+# 2-4
+print(name.lower())
+print(name.upper())
+print(name.title())
+'''
+
+'''
+# 2-5
+author = "most people"
+quote = 'shit happens'
+print(author.title() + ' often say, "' + quote.upper() + '."')
+
+# 2-6
+famous_words = author.title() + ' often say, "' + quote.upper() + '."'
+print(famous_words)
+'''
+
+'''
+# 2-7
+new_name = "   Joa\thny C\nordaria       P  .   "
+print(new_name)
+print(new_name.lstrip())
+print(new_name.rstrip())
+print(new_name.strip())
+'''
+
+# section 2 numbers
+# 2-8
+print(5+3)
+favorite_number = 27
+
+# ran into problem printing combining number and string together throwing error
+# solution: str() func casts a number to a string when needing to print
+print(str(favorite_number) + ' is my favorite number')
+
+# 2-10
+# this one is to add comments to code, which I have been doing the whole time.
+
+# 2-11
+# this was to "import this" in the python console
+# it prints out a little note from a creator of python 
 
