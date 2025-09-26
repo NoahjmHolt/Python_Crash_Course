502ec902f834f08d7aef875f708409e7284bd180
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2025-09-26 16:59:50 -0600

    Keeping on chap 3
    Keeping working on Chapter 3 working and manipulating lists.
    
    One problem encountered. Found solution sort of quick though so thats nice

--- chap3TIY.py
+++ chap3TIY.py
@@ -2,7 +2,7 @@
 # section 1 Lists
 
 # 3-1
-
+'''
 friends = ["ME", "Tanner", "Riley", "Owen"]
 print(friends[1])
 print(friends[3])
@@ -11,10 +11,47 @@ print(friends)
 # 3-2
 for friend in friends:
     print("Hello, " + friend)
+'''
 
-
+'''
 # 3-3
 newList = ["stuff", "things", 'fiz', 'buz']
+
+# 3-4
+historical = ["Abe", "Joe", "Jeff"]
+for famous in historical:
+    print("Come to dinner this Friday, " + famous)
+
+# 3-5
+print('\njoe cant come\n')
+historical[1] = 'joeseph'
+for famous in historical:
+    print("Come to dinner this Friday, " + famous)
+
+# 3-6
+print('\nThere is a bigger table, who else should come?\n')
+
+historical.insert(0, 'Thomas')
+historical.insert(2, 'levi')
+historical.append('Tyler')
+for famous in historical:
+    print("Come to dinner this Friday, " + famous)
+
+# 3-7
+print('\nTable wont make it in time\n')
+
+listSize = len(historical)
+# print(listSize)   Was using size of and it wasnt giving the len of the list so had to switch things up
+while listSize > 2:
+    historical.pop()
+    listSize = len(historical)
+
+for famous in historical:
+    print("Come to dinner this Friday, " + famous)
+    
+'''
+
+# 3-8
 
 
 
