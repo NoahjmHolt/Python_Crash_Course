481c3188fbab4f374854c5cc26b3d027499bd4c6
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2025-09-25 18:08:46 -0600

    Begiinning chapter 3
    Started with the first few problems from chapter 3

---
+++ chap3TIY.py
@@ -0,0 +1,20 @@
+
+# section 1 Lists
+
+# 3-1
+
+friends = ["ME", "Tanner", "Riley", "Owen"]
+print(friends[1])
+print(friends[3])
+print(friends)
+
+# 3-2
+for friend in friends:
+    print("Hello, " + friend)
+
+
+# 3-3
+newList = ["stuff", "things", 'fiz', 'buz']
+
+
+
