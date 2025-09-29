b2a1067b793ed09f1d331f98242c372eef607ad7
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2025-09-29 17:56:44 -0600

    4-3 and 4-4
    Simple enough, creating lists of different quantaties, though both I did wrong to start and had to fix. Notes at bottom of file

--- chap4TIY.py
+++ chap4TIY.py
@@ -9,6 +9,7 @@ What its about:
 
 # Prompts and project notes
 
+'''
 # Section 1
 # 4-1
 pizza = ["chicken & Jalepenos",  "Supreme", "pepperoni", "Cheese", "Veggie", "Meat Lovers"]
@@ -24,10 +25,27 @@ for bear in animals:
     print(f"Well Forrest, there are all types of bear like {bear}...")
 
 print("all these animals are called bears")
+'''
 
+# Section 2
+# 4-3
+for digit in range(20):
+    print(digit + 1)
+
+# 4-4
+large = [digit for digit in range(1, 1000001)]
+print(large)
 
 # Errors and fixes
 
+# 4-3
+    # not really an error but  to print numbers with range
+    # it starts at 0 and ends 1 digit before your asked one
+    # so to print, I added 1 in the print box
+
+# 4-4
+    # to add the list of all the numbers you need to do the
+    # little for loop inside, cant just say [range()]
 
 
 
