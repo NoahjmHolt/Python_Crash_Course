767cd6967edf16b67dfcf9ca17750f5c05dcaf13
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2025-09-29 17:46:44 -0600

    4-1
    Finished with 4-1 and moving forward

---
+++ chap4TIY.py
@@ -0,0 +1,27 @@
+'''
+Chapter 4: Working with Lists
+
+What its about:
+    Looping with lists and working with them as a whole
+    instead of each element like last chapter
+
+'''
+
+# Prompts and project notes
+
+# Section 1
+# 4-1
+pizza = ["chicken & Jalepenos",  "Supreme", "pepperoni", "Cheese", "Veggie", "Meat Lovers"]
+
+for za in pizza:
+    print(f"One choice of pizza is: {za}")
+
+print("Pizza is pretty versitile.")
+
+
+
+# Errors and fixes
+
+
+
+
