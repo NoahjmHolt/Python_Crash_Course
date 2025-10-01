ff8242d533af68f0545867244e1568ff1e8ef964
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2025-10-01 17:41:41 -0600

    Chapter 5
    read all the exercises for chapter 5, it was just if statements then moved to else, then off to elifs.
    
    I have written probably 20+ over the last week or so and so decided to skip for now, will return if needed.

---
+++ chap5TIY.py
@@ -0,0 +1,34 @@
+
+'''
+Chapter 5: if statemends
+
+What its about:
+    looping thorugh stuff with different things
+
+Im gonna be honest I have been writing ifs and fors and whiles the whole week
+with boot.dev and other solutions from this book.
+Not sure why this is so far back in the book but I know how this works
+so am scimming and moving on.
+
+'''
+
+# 5-1
+# examining conditional statements and what the result in
+
+# 5-2
+# same as 5-1
+
+# 5-3
+# checking the color of something and if elif else to do different things.
+# 5-4
+# correction, 5-4 is the elif, 5-3 was several seperate if statements
+
+# 5-5
+# correction again 5-4 was if elses, 5-5 is elif
+
+# 5-6
+# doing the same thing but with numbers now
+
+# 5-7
+# similar to 5-2
+
